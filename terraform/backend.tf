b70a7eb23c2d1b58971e8f0232ed5c85c09abacf
21E51A <akashpaiavula2003@gmail.com>
2025-12-24 20:23:19 +0530

    update

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "ecommerce-terraform-state-24-12-2025"
-    key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
+    bucket       = "ecommerce-terraform-state-24-12-2025"
+    key          = "prod/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
